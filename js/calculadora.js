636ad6299c390371fa1b6f18a05f1c45c20fe11e
Lucas <lucas.laborne@gmail.com>
2019-11-12 20:53:13 -0300

    ok

---
+++ js/calculadora.js
@@ -0,0 +1,7 @@
+
+
+// Calcula a quantidade (em Litros) de água
+function calcularGasto() {
+    $('#litros-gastos').html('20 Litros');
+    $('#mensagem-gastos').html('Parabéns, vc consome água pa caraio');
+}
