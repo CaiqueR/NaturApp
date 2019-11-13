ce60034a0434c299bcbc53d267a5b7c1d8a3950a
Lucas <lucas.laborne@gmail.com>
2019-11-13 18:41:29 -0300

    ok

--- js/agenda.js
+++ js/agenda.js
@@ -1,4 +1,8 @@
 
+const sugestoes = [
+    '<a href="">Cultive uma Horta Orgânica<a>',
+];
+
 
 // Valida os inputs (Se o tempo A < B, e se já não existe o horário informado)
 function validarTempoLivre(item) {
@@ -11,12 +15,26 @@ function validarTempoLivre(item) {
 
 // Obtem a lista de tempos livres
 function getTemposLivres() {
-    return $('#tabela-tempo-livre tr');
+    return $('#tabela-tempo-livre tr').splice(1);
 }
 
 // Remove um horário disponível
 function removerTempoLivre(itemId) {
     $('#' + itemId).remove();
+    let temposLivres = getTemposLivres();
+    if (temposLivres.length == 0) {
+        $('#div-tabela-tempo-livre').attr('hidden', true);
+    }
+}
+
+function gerarAgenda() {
+    let temposLivres = getTemposLivres();
+    if (temposLivres.length != 0) {
+        $('#label-tempo-livre').html('Sugestões');
+        $('.botao-tempo-livre').each(function (index) {
+            $(this).html('teste123');
+        });
+    }
 }
 
 // Adiciona um item na lista de tempos disponiveis
@@ -29,7 +47,7 @@ function addTempoLivre() {
     if (validarTempoLivre(item)) {
         let id = (new Date).getTime();
         $('#tabela-tempo-livre').append('<tr id="' + id + '"><td>' + item.dia + '</td><td>' + item.de + '</td><td>'
-            + item.as + '</td><td><button class="btn-danger" onclick="removerTempoLivre(' + id + ')">Remover</button></td></tr>');
+            + item.as + '</td><td class="botao-tempo-livre"><button class="btn-danger" onclick="removerTempoLivre(' + id + ')">Remover</button></td></tr>');
         if ($('#div-tabela-tempo-livre').attr('hidden')) {
             $('#div-tabela-tempo-livre').attr('hidden', false);
         }
