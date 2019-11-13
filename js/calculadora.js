272d05a19cb9dc349dede8a9a7a5298ababd8e3a
Caique RIbeiro <caiiquexd@gmail.com>
2019-11-13 17:54:23 -0300

    Arrumada a calculadora de agua
    Arrumada a calculadora de agua

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,7 +1,19 @@
 
-
 // Calcula a quantidade (em Litros) de água
 function calcularGasto() {
-    $('#litros-gastos').html('20 Litros');
-    $('#mensagem-gastos').html('Parabéns, vc consome água pa caraio');
+
+    if (!isEmpty($("#minutos-banho")[0].value) && !isEmpty($("#minutos-pia")[0].value)) {
+        let consumobanho = $("#minutos-banho")[0].value * 12;
+        let consumopia = $("#minutos-pia")[0].value * 6;
+        $('#litros-gastos').html(`${consumobanho + consumopia} litros`);
+        $('#mensagem-gastos').html('Parabéns, vc consome água pa caraio');
+    }
+}
+
+function isEmpty(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
 }
